ea3674878954709308242b67f6f24db3f13b98f4
s2ddv <1speedftn@gmail.com>
2025-05-15 15:09:14 -0300

    feat: add features

--- django_api/django_api/livros/urls.py
+++ django_api/django_api/livros/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from . import views
 from django.urls import path
 from . import views
+from .views import livro_detalhado
 
 urlpatterns = [
+    path('livros/<int:id>/', livro_detalhado, name='livro_detalhado'),
     path('generos/', views.GeneroList.as_view(), name='genero-list'),
     path('generos/<int:pk>/', views.GeneroDetail.as_view(), name='genero-detail'),
     path('livros/', views.LivroList.as_view(), name='livro-list'),
--- django_api/django_api/livros/views.py
+++ django_api/django_api/livros/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Livro
 
 from rest_framework import generics
 from .models import Genero, Livro
@@ -16,3 +18,6 @@ class LivroList(generics.ListCreateAPIView):
 class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = Livroserializer
+def livro_detalhado(request, id):
+    livro = get_object_or_404(Livro, id=id)
+    return render(request, 'livro_detalhado.html', {'livro': livro})
