9bfa174f2248388105b167fd8b72c97742582b92
s2ddv <1speedftn@gmail.com>
2025-05-28 18:18:00 -0300

    docs: organize files

--- django_api/django_api/core/settings.py
+++ django_api/django_api/core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
+    "http://127.0.0.1:3000",
 ]
 # Application definition
 
@@ -60,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_api.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_api.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
